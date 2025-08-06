0482126ef713dd67dc1814c46edb22a0981ad30e
cristiancadiz <cmcadiz@ing.ucsc.cl>
2025-08-05 20:39:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -292,5 +292,10 @@ TEMPLATE = '''
 </html>
 '''
 
+import os
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
