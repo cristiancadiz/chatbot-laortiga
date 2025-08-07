55d428f99663055ffc6047a3035cd679149a5b4c
cristiancadiz <cmcadiz@ing.ucsc.cl>
2025-08-06 22:04:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -111,4 +111,7 @@ TEMPLATE = """
 """
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Lee el puerto desde la variable de entorno o usa 5000 por defecto
+    app.run(host='0.0.0.0', port=port, debug=True)  # Escucha en todas las interfaces
+
+
