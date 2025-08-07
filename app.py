0d9e48eb6e184b1dd6d3f9220dfff7747b679d41
cristiancadiz <cmcadiz@ing.ucsc.cl>
2025-08-07 01:44:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -91,4 +91,5 @@ def create_event():
         return f"Hubo un error al crear el evento: {e}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
